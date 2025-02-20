7fb6c4d56667ea5aac26ecfff88fb5072ff3732b
nell124 <nailahh769@gmail.com>
2025-02-20 12:32:19 +0700

    praktikum1

---
+++ variable_system.php
@@ -0,0 +1,5 @@
+<?php
+echo "DOKUMENT ROOT : " . $_SERVER["DOCUMENT_ROOT"];
+
+
+?>
---
+++ variable_user.php
@@ -0,0 +1,14 @@
+<?php
+//mendefinisikan variable
+$nama = 'nela';
+$umur = 19;
+$berat = 50;
+
+//untuk mencetak menggunakan echo
+echo 'Nama saya ' . $nama;
+echo '<br/>umur ' . $umur . 'Tahun';
+echo '<br/>berat ' . $berat;
+
+//<br/> untuk membuat baris baru
+//bedanya dengan konstanta tidak bisa diubah2
+?> 
